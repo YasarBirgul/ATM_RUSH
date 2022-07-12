18f60a1a313b154640ae546acb283ee8bb16a2e6
Yaşar Birgül <yasarbrgul@gmail.com>
2022-07-12 13:36:51 +0300

    Collectable Scripts Added

---
+++ Assets/Scripts/Controllers/CollectablePhysicsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Managers;
+using Signals;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class CollectablePhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+        #region Public Variables
+        #endregion
+        #region Serialized Variables
+        [SerializeField] private CollectableManager collectableManager;
+        [SerializeField] private new Rigidbody rigidbody;
+        [SerializeField] private new Collider Collider;
+        #endregion
+        #endregion
+        private void OnTriggerEnter(Collider other)
+        { 
+            if(other.gameObject.CompareTag("Collectable"))
+            {
+                CollectableSignals.Instance.onMoneyCollection?.Invoke();
+                other.gameObject.transform.position = transform.position + Vector3.forward;
+                other.gameObject.tag = "Collected";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Signals/CollectableSignals.cs
@@ -0,0 +1,14 @@
+﻿using Extentions;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class CollectableSignals : MonoSingleton<CollectableSignals>
+    { 
+        public UnityAction onMoneyCollection=delegate {  };
+        public UnityAction onObstacleCollision=delegate {  };
+        public UnityAction onUpgradeMOney=delegate {  };
+        public UnityAction onChangeState=delegate {  };
+        public UnityAction onDeposit=delegate {  };
+    }
+}
