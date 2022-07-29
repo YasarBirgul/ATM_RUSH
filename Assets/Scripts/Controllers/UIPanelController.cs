43ce917f82324f635c2192338c92aa4224f5eda4
gokhansaldik <gokhansaldik@gmail.com>
2022-07-29 17:14:34 +0300

    interface bug fixed

--- Assets/Scripts/Controllers/UIPanelController.cs
+++ Assets/Scripts/Controllers/UIPanelController.cs
@@ -12,8 +12,10 @@ namespace Controllers
         #region Serialized Variables
 
         [SerializeField] private List<GameObject> panels;
-        
-
+        [SerializeField] private GameObject noAdsButton;
+        [SerializeField] private GameObject shopButton;
+        [SerializeField] private GameObject settingsButton;
+        [SerializeField] private GameObject layoutGroup;
         #endregion
         #region Private Variables
 
@@ -24,12 +26,19 @@ namespace Controllers
         public void OpenPanel(UIPanels panelParam)
         {
             panels[(int) panelParam].SetActive(true);
+            noAdsButton.SetActive(true);
+            shopButton.SetActive(true);
+            settingsButton.SetActive(true);
+            layoutGroup.SetActive(true);
         }
 
         public void ClosePanel(UIPanels panelParam)
         {
             panels[(int) panelParam].SetActive(false);
-            
+            noAdsButton.SetActive(false);
+            shopButton.SetActive(false);
+            settingsButton.SetActive(false);
+            layoutGroup.SetActive(false);
         }
     }
 }
