edb6e3b5ff752a2ce4603f579c4483101191c1ae
Yaşar Birgül <yasarbrgul@gmail.com>
2022-08-08 09:21:00 +0300

    Changes

--- Assets/Scripts/Managers/CollectableParticleManager.cs
+++ Assets/Scripts/Managers/CollectableParticleManager.cs
@@ -1,8 +1,10 @@
-﻿using Data.UnityObject;
+﻿using System;
+using Data.UnityObject;
 using Data.ValueObject;
 using Enums;
 using Signals;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Managers
 {
@@ -14,7 +16,7 @@ namespace Managers
 
         public CollectableData Data;
 
-        public ParticleSystem system;
+        public ParticleSystem ParticleSystem;
 
         #endregion
 
@@ -44,7 +46,7 @@ namespace Managers
         private void UnsubscribeEvents()
         {
             CollectableSignals.Instance.onObstacleCollision -= OnObstacleCollision;
-            CollectableSignals.Instance.onDeposit += OnDeposit;
+            CollectableSignals.Instance.onDeposit -= OnDeposit;
         }
 
         private void OnDisable()
@@ -54,23 +56,42 @@ namespace Managers
 
         #endregion
 
+
+        private void Awake()
+        {
+            Data = GetParticleData();
+            gameObject.AddComponent<ParticleSystem>();
+            ParticleSystem = GetComponent<ParticleSystem>();
+        }
+
         private void Start()
         {
-
-            Data = GetParticleData();
+            Material particleMaterial = new Material(Shader.Find("Universal Render Pipeline/Particles/Unlit"));
             
-           Material particleMaterial = new Material(Shader.Find("Universal Render Pipeline/Unlit"));
-            var go = gameObject;
-            go.transform.Rotate(-90, 45, 0); // Rotate so the system emits upwards.
-            system = go.GetComponent<ParticleSystem>();
-            // go.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
-            var mainModule = system.main;
-            mainModule.gravityModifier = 0;
+            transform.Rotate(0, 0, 0);
+            gameObject.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
+            
+            var mainModule = ParticleSystem.main;
+            var textureModule = ParticleSystem.textureSheetAnimation;
+            var emissionModule = ParticleSystem.emission;
+            var shapeModule = ParticleSystem.shape;
+            
+            ParticleSystem.Stop();
+            mainModule.duration = 0.4f;
+            mainModule.startLifetime = 1f;
+            mainModule.gravityModifier = 0.4f;
             mainModule.loop = false;
+            mainModule.playOnAwake = false;
             mainModule.startSize = 0.5f;
             mainModule.maxParticles = 10;
-
-            // Every 2 secs we will emit.
+            
+            textureModule.enabled = true;
+            textureModule.mode = ParticleSystemAnimationMode.Sprites;
+            
+            emissionModule.rateOverTime = 0;
+            emissionModule.burstCount = 10;
+            
+            
         }
 
         public CollectableData GetParticleData() => Resources.Load<CD_Collectable>("Data/CD_Collectable").CollectableData;
@@ -86,22 +107,22 @@ namespace Managers
 
               if (ColObjStateData == CollectableType.Money)
               {
-                  system.textureSheetAnimation.SetSprite(0,particleSprite);
+                  ParticleSystem.textureSheetAnimation.SetSprite(0,particleSprite);
                   
-                  DoEmit();
+                  DoEmit(particleSprite);
               }
               if (ColObjStateData == CollectableType.Gold)
               {
-                  system.textureSheetAnimation.SetSprite(0,particleSprite);
+                  ParticleSystem.textureSheetAnimation.SetSprite(0,particleSprite);
                   
-                 DoEmit();
+                 DoEmit(particleSprite);
                   
               }
               if (ColObjStateData == CollectableType.Diamond)
               {
-                  system.textureSheetAnimation.SetSprite(0,particleSprite);
+                  ParticleSystem.textureSheetAnimation.SetSprite(0,particleSprite);
                   
-                  DoEmit();
+                  DoEmit(particleSprite);
               }
         }
          private void OnDeposit(GameObject CollidedActiveObject,int ID)
@@ -110,20 +131,21 @@ namespace Managers
              
              if (CollidedActiveObject.CompareTag("Collected"))
              {
-                 system.textureSheetAnimation.SetSprite(0,particleSprite);
+                 ParticleSystem.textureSheetAnimation.SetSprite(0,particleSprite);
                  
                  transform.position = CollidedActiveObject.GetComponent<Collider>().transform.position;
                
-                 DoEmit();
+                 DoEmit(particleSprite);
              }
          }
 
-         void DoEmit()
+         void DoEmit(Sprite particleSprite)
          {
+             var TextureParams =  ParticleSystem.textureSheetAnimation;
              var emitParams = new ParticleSystem.EmitParams();
-             emitParams.startSize = 1f;
-             system.Emit(emitParams, 10);
-             system.Play();
+             TextureParams.SetSprite(0,particleSprite);
+             ParticleSystem.Emit(emitParams, 10);
+             ParticleSystem.Play();
          }
     }
 }
